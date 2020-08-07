7d8b995defc76d676c403ff2118ee8091a3653dc
Ailton Muniz Santos Junior <ailton.msj@gmail.com>
2020-08-07 11:31:42 -0300

    Added control under healthcheck

--- src/main/java/br/com/amsj/infrastructure/autoscale/controller/HealthCheck.java
+++ src/main/java/br/com/amsj/infrastructure/autoscale/controller/HealthCheck.java
@@ -1,7 +1,10 @@
 package br.com.amsj.infrastructure.autoscale.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class HealthCheck {
 	
+	private boolean healthcheckstatus = true; 
+	
 	@GetMapping(path = "/")
 	public ResponseEntity<String> healthcheck(){
+		
+		if(this.healthcheckstatus) {
+			return ResponseEntity.ok().build();
+		}else {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+	
+	@PutMapping(path = "/{healthcheckstatus}")
+	public ResponseEntity<Void> healthcheck(@PathVariable(name = "healthcheckstatus", required = true) boolean healthcheckstatus){
+		
+		this.healthcheckstatus = healthcheckstatus;
 		
 		return ResponseEntity.ok().build();
 	}
